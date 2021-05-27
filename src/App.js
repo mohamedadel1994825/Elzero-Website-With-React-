6217c178b87c8928f21c38d5d6a5fddf53d39b20
mohamedadel1994825 <mohamedadel1994825@gmail.com>
2021-05-27 14:38:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,324 @@
-import logo from './logo.svg';
-import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import 'bootstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+        </link>
+      </head>
+      <div class="upper-bar">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <i class="fas fa-phone-square-alt"></i>
+              <span>0000000000</span>
+              <i class="fas fa-envelope-square"></i>
+              <span> osama212.com </span>
+            </div>
+            <div class="col-sm quote-cont">
+              <span>Let's work together</span>
+              <span class="get-qute"> Get Quote</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <span>Elite</span>
+            <span>Crop</span>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mainnav"
+            aria-controls="mainnav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="mainnav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Home{" "}
+                </a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  About
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Work
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Blog
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input
+                class="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                class="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div id="main-slider" class="carousel slider" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#main-slider" data-slide-to="0" class="active"></li>
+          <li data-target="#main-slider" data-slide-to="1"></li>
+          <li data-target="#main-slider" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <h1>
+            We are independent Design and Web <span>Development</span>
+            <br />
+            Agency
+          </h1>
+          <div class="overlay"></div>
+          <div class="carousel-item active carousel-one"></div>
+          <div class="carousel-item carousel-two"></div>
+          <div class="carousel-item carousel-three"></div>
+        </div>
+      </div>
+      <div class="features text-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6 col-md-6 col-lg-3">
+              <i class="fas fa-phone-square-alt fa-2x rounded-circle"></i>
+              <h3>Greate Idea</h3>
+              <p>
+                simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since
+              </p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-3">
+              <i class="fas fa-home fa-2x rounded-circle"></i>
+              <h3>Greate Idea</h3>
+              <p>
+                simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since
+              </p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-3">
+              <i class="fas fa-phone-square-alt fa-2x rounded-circle"></i>
+              <h3>Greate Idea</h3>
+              <p>
+                simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since
+              </p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-3">
+              <i class="fas fa-phone-square-alt fa-2x rounded-circle"></i>
+              <h3>Greate Idea</h3>
+              <p>
+                simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="companyOverview">
+        <div class="container text-center">
+          <h2>Company OverView</h2>
+          <p>
+            simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since
+          </p>
+          <h5>Let's Start Today</h5>
+          <button>View More</button>
+        </div>
+      </div>
+      <div class="featured-work text-center mt-5">
+        <div class="container">
+          <h2>Featured Work</h2>
+          <p>
+            simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since
+          </p>
+          <ul class="list-unstyled row mt-5">
+            <li class="col-md active">All</li>
+            <li class="col-md">Website</li>
+            <li class="col-md">Logo</li>
+            <li class="col-md">graphic</li>
+            <li class="col-md">online</li>
+            <li class="col-md">video</li>
+          </ul>
+        </div>
+        <div class="shuffleImages">
+          <div class="row my-5">
+            <div class="col-md">
+              <img
+                class="website"
+                src="https://placeimg.com/400/400/animals  "
+                alt="..."
+              />
+            </div>
+            <div class="col-md">
+              <img
+                src="https://placeimg.com/400/400/animals  "
+                alt="..."
+                class="logos"
+              />
+            </div>
+            <div class="col-md">
+              <img
+                src="https://placeimg.com/400/400/any  "
+                alt="..."
+                class="online"
+              />
+            </div>
+            <div class="col-md">
+              <img
+                src="https://placeimg.com/400/400/tech  "
+                alt="..."
+                class="video"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md">
+              <img
+                src="https://placeimg.com/400/400/arch  "
+                alt="..."
+                class="video"
+              />
+            </div>
+            <div class="col-md">
+              <img
+                src="https://placeimg.com/400/400/nature  "
+                alt="..."
+                class="graphics"
+              />
+            </div>
+            <div class="col-md">
+              <img
+                src="https://placeimg.com/400/400/tech  "
+                alt="..."
+                class="website"
+              />
+            </div>
+            <div class="col-md">
+              <img
+                class="graphics"
+                src="https://placeimg.com/400/400/sepia  "
+                alt="..."
+              />
+            </div>
+          </div>
+        </div>
+        <div class="latestPost mt-5 pb-5 text-center">
+          <div class="container">
+            <h2>Latest Posts</h2>
+            <p class="paragraphTitle">
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since
+            </p>
+            <div class="cards row justify-content-center  ">
+              <div class="card text-left col-lg col-md-6 mb-3">
+                <img
+                  src="https://placeimg.com/400/400/sepia  "
+                  class="card-img-top "
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">
+                    Lorem Ipsum is simply dummy text of the printing and
+                  </h5>
+                  <h6 class="card-subtitle mb-2 text-muted">March 24 2021</h6>
+
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content Some quick example text.
+                  </p>
+                  <a href="#" class="btn text-left mt-2">
+                    Read More
+                  </a>
+                </div>
+              </div>
+              <div class="card text-left col-lg col-md-6 mb-3">
+                <img
+                  src="https://placeimg.com/400/400/sepia  "
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">
+                    Lorem Ipsum is simply dummy text of the printing and
+                  </h5>
+                  <h6 class="card-subtitle mb-2 text-muted">March 24 2021</h6>
+
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content Some quick example text.
+                  </p>
+                  <a href="#" class="btn text-left mt-2">
+                    Read More
+                  </a>
+                </div>
+              </div>
+              <div class="card text-left col-lg col-md-6 mb-3">
+                <img
+                  src="https://placeimg.com/400/400/sepia  "
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">
+                    Lorem Ipsum is simply dummy text of the printing and
+                  </h5>
+                  <h6 class="card-subtitle mb-2 text-muted">March 24 2021</h6>
+
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content Some quick example text.
+                  </p>
+                  <a href="#" class="btn text-left mt-2">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
